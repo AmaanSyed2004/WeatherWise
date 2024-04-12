462ca241ea3d22c344a0aea43e5fde4412067550
AmaanSyed2004 <amaansyed2004@gmail.com>
2024-04-13 00:51:49 +0530

    initialize backend

---
+++ backend/index.js
@@ -0,0 +1,8 @@
+import express from "express"
+import dotenv from 'dotenv'
+dotenv.config();
+const app= express();
+const port= 3000;
+app.listen(port, ()=>{
+    console.log(`Listening on port ${port}`)
+})
